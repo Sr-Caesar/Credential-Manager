7f5d89508602888ecb7f9a49ac6cf456527a2733
Giulio <giulio.sanna@outlook.it>
2023-05-08 08:40:23 +0200

    Add Create Account DB file => SQLQuery1

---
+++ SQLQuery1.sql
@@ -0,0 +1,11 @@
+CREATE DATABASE Account
+GO
+USE Account
+GO
+
+CREATE TABLE Account (
+Matricola INT NOT NULL IDENTITY(1,1) PRIMARY KEY,
+Username VARCHAR(100) NOT NULL,
+Password VARCHAR(100) NOT NULL,
+SubscriptionDate DATE
+)
